ca8cca02e9b29884595e6840f560438c8f048485
Ktssatwik <ktssatwik@gmail.com>
2025-06-05 19:33:08 +0530

    updates

--- p2/script.js
+++ p2/script.js
@@ -3,12 +3,18 @@ const showLogin = () =>
     let str = `
     <div class='App-Container'>
     <h1>LOGIN FORM </h1>
-    <p><input type="email" id="txtemail" placeholder="email"></P>
-    <p><input type="password" id="txtpass" placeholder="password"></P>
-    <p><button>LOG IN</button>
+    <p><label for="email">ENTER EMAIL</LABEL> <input type="email" id="txtemail" placeholder="email"></P>
+    <p><label for="pass">ENTER PASSWORD</LABEL> <input type="password" id="txtpass" placeholder="password"></P>
+    <p><button onclick="showWelcome()">LOG IN</button>
     <p><button onclick="showRegister()">REGISTER</button>
     </div>
     `
+    root.innerHTML = str;
+}
+
+const showWelcome =()=>
+{
+    let str=`<h1> welcome </h1>`
     root.innerHTML = str;
 }
 
@@ -16,10 +22,16 @@ const showRegister = () =>
 {
     let str = `
     <h1>CREATE ACCCOUNT</h1>
-    <p><input type="text" id="txtname" placeholder="name"></P>
-    <p><input type="email" id="txtemail" placeholder="email"></P>
-    <p><input type="password" id="txtpass" placeholder="password"></P>
-    <p><button onclick="showLogin()">REGISTOR</button></p>
+    <p><label for="name">ENTER NAME</LABEL> 
+    <input type="text" id="txtname" placeholder="name"></P>
+
+    <p><label for="email">ENTER EMAIL</LABEL> 
+    <input type="email" id="txtemail" placeholder="email"></P>
+    
+    <p><label for="pass">ENTER PASSWORD</LABEL> 
+    <input type="password" id="txtpass" placeholder="password"></P>
+    <p><button>REGISTOR</button></p>
+    <p><button onclick="showLogin()">ALREADY HAVE AN ACCOUNT?</button></p>
     `
     root.innerHTML = str;
 }
